f56aba6b2e50e2264eff7aa23c0a604c29d22067
Anton Baula <anton.baula@gmail.com>
2022-07-08 19:32:43 +0300

    Оаисание для методов BoxLibfactory.

--- src/BoxLibFactory.cs
+++ src/BoxLibFactory.cs
@@ -4,22 +4,53 @@ namespace BoxLib
 {
     public static class BoxLibFactory
     {
+        /// <summary>
+        /// Создаёт экземпляр настроек из файла.
+        /// </summary>
+        /// <param name="path">Путь к файлу настроек.</param>
+        /// <returns>Экземпляр настроек.</returns>
         public static ValueTask<BoxSettings> LoadSettings(string path)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Создаёт новый Box.
+        /// </summary>
+        /// <param name="settings">Экземпляр настроек.</param>
+        /// <returns>Ссылка на объект BoxLib.</returns>
+        /// <exception cref="System.InvalidOperationException">Ошибка при создании Box.</exception>
         public static ValueTask<IBoxLib> Create(BoxSettings settings)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Создаёт новый Box.
+        /// </summary>
+        /// <param name="settingsPath">Путь к файлу настроек.</param>
+        /// <returns>Ссылка на объект BoxLib.</returns>
+        /// <exception cref="System.InvalidOperationException">Ошибка при создании Box.</exception>
         public static ValueTask<IBoxLib> Create(string settingsPath)
         {
             throw new NotImplementedException();
         }
 
-        public static ValueTask<IBoxLib> Get(string settingsPath)
+        /// <summary>
+        /// Открывает существующий Box.
+        /// </summary>
+        /// <param name="settingsPath">Путь к файлу настроек.</param>
+        /// <returns>Ссылка на объект BoxLib.</returns>
+        public static ValueTask<IBoxLib> Open(string settingsPath)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Удаляет все данные Box.
+        /// </summary>
+        /// <param name="settingsPath">Путь к файлу настроек.</param>
+        public static ValueTask Delete(string settingsPath)
         {
             throw new NotImplementedException();
         }
